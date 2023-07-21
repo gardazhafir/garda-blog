1ed0f3284fc4f75bcb3a96d8f5fe12e48ef1d95d
gardazhafir <rmgardazhafir@gmail.com>
2023-07-21 14:18:49 +0700

    Change to welcome message and edit some css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <Navbar />
       <div className="content">
         <h1> Welcome </h1>
+        <h2>Hi, My name is Garda. I'm 21 and right now I'm a computer science student at Universitas Gadjah Mada</h2>
       </div>
     </div>
   );
