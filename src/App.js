22e12ab679cd05a22630252905f5b1e911784e88
gardazhafir <rmgardazhafir@gmail.com>
2023-07-21 13:42:56 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './Home';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <h1> Welcome </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
