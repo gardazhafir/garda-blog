11099f6e6e5903176ff127ee2e735c66bcf14636
gardazhafir <rmgardazhafir@gmail.com>
2023-09-04 17:35:03 +0700

    Insert logo and changing bg color

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import logo from './assets/images/Logo_G.png'; 
+
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Portofolio Garda</h1>
+            <div className='pojok'>
+                <a href="/"><img src={logo} alt="Logo"></img></a>
+            </div>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/resume"><b>Resume</b></a>
+                <a href="/project"><b>Project</b></a>
             </div>
         </nav>
       );
